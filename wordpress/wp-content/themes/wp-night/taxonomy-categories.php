ca82fbecf6e8d780b314b7a5c27c2c649322e79f
crazyyy <crazyyy@gmail.com>
2016-08-07 12:58:06 +0300

    acf updated

--- wordpress/wp-content/themes/wp-night/taxonomy-categories.php
+++ wordpress/wp-content/themes/wp-night/taxonomy-categories.php
@@ -7,12 +7,12 @@
       <div class="col-md-3 cat-exc-cal excursion-cat-zero">
         <?php get_template_part('inc-calendar'); ?>
       </div><!-- /.col-md-3 cat-exc-cal -->
-      <div class="col-md-9 cat-exc-choos excursion-level-zero">
+      <div class="col-md-9 cat-exc-choos-disabled excursion-level-zero">
         <h4>Выберите вид экскурсии</h4>
         <ul>
           <?php $terms = get_terms( array( 'taxonomy' => 'categories', 'hide_empty' => false,) ); foreach ($terms as $term) { ?>
             <li>
-              <a data-type="<?php echo $term->slug; ?>" href="#">
+              <a href="<?php echo home_url(); ?>/types/<?php echo $term->slug; ?>">
                 <?php $image = get_field('image', $term );  ?>
                   <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
                 <?php ?>
@@ -20,7 +20,7 @@
               <span><?php echo $term->name; ?></span>
             </li>
             <?php } ?>
-          <li><a data-type="ekskursii-dlya-inostrantsev" href="#">Экскурсии для иностранцев</a></li>
+          <li><a href="<?php echo home_url(); ?>/types/ekskursii-dlya-inostrantsev">Экскурсии для иностранцев</a></li>
         </ul>
       </div><!-- /.col-md-9 cat-exc-choos -->
 
